34418f5f95ef675befa315e8134126d6870a4594
Nawaritta <Nor.benhaddou@gmail.com>
2024-03-08 21:56:06 +0000

    redis server

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,54 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+        this.client.get(key, (error, reply) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(reply);
+            }
+        });
+    });
+}
+
+async set(key, value) {
+    return new Promise((resolve, reject) => {
+        this.client.set(key, value, (error, reply) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(reply);
+            }
+        });
+    });
+}
+
+  async del(key) {
+    return new Promise((resolve, _reject) => {
+      this.client.del(key, (error) => {
+        if (error) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
 
