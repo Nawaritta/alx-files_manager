002963518d760bb747f2cb772f22e474d3826534
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-09 06:47:51 +0100

    Task1 MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,32 @@
 
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (err) {
+        this.dbClient = false;
+      }
+      this.dbClient = client.db(database);
+    });
+  }
+
+  isAlive() {
+    return this.dbClient && this.dbClient.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+export default new DBClient();
