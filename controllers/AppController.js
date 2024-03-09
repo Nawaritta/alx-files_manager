30938653a90b18ca0a766341fec65ae1a49e09eb
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-09 06:07:30 +0100

    Task2 First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,19 @@
 
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
+
+class AppController {
+  static getStatus(req, res) {
+    const redisLive = redisClient.isAlive();
+    const dbLive = dbClient.isAlive();
+    res.status(200).json({ redis: redisLive, db: dbLive });
+  }
+
+  static async getStats(req, res) {
+    const usersTotal = await dbClient.nbUsers();
+    const filesTotal = await dbClient.nbFiles();
+    res.status(200).json({ users: usersTotal, files: filesTotal });
+  }
+}
+
+export default AppController;
