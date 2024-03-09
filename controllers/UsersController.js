3ee8371b13821248fa6085e1692dc21bb548e927
Nawaritta <Nor.benhaddou@gmail.com>
2024-03-09 18:00:14 +0000

    create a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,27 @@
+import dbClient from '../utils/db';
+import sha1 from 'sha1';
 
+const UsersController = {
+  async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).send('{"error": "Missing email"}');
+    }
+    if (!password) {
+      return res.status(400).send('{"error": "Missing password"}');
+    }
+
+    const userExists = await dbClient.db().collection('users').findOne({ email });
+    if (userExists) {
+      return res.status(400).send('{"error": "Already exist"}');
+    }
+
+    const hashedPassword = sha1(password);
+
+    const result = await dbClient.db().collection('users').insertOne({ email, password: hashedPassword });
+    return res.status(201).send(`{ id: ${result.insertedId}, ${email} }`);
+  }
+};
+
+export default UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 
 import express from 'express';
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
