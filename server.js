dc053ed90cca443f33fd14f8e31bc1db72060ee4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-10 02:37:00 +0000

    add express.json() to parse json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.use('/', routes);
