f28ecf4797439ea2b5a5d2946812d95dc89d3326
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-10 04:57:37 +0000

    Task4 Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,10 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+import { ObjectId } from 'mongodb';
 
-const UsersController = {
-  async postNew(req, res) {
+class UsersController {
+  static async postNew(req, res) {
     const { email, password } = req.body;
 
     if (!email) {
@@ -20,8 +22,21 @@ const UsersController = {
     const hashedPassword = sha1(password);
 
     const result = await dbClient.dbClient.collection('users').insertOne({ email, password: hashedPassword });
-    return res.status(201).send(`{ id: ${result.insertedId}, ${email} }`);
-  },
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+
+    const userId = await redisClient.get(`auth_${token}`);
+    console.log(userId)
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.dbClient.collection('users').findOne({ _id: new ObjectId(userId) });
+    return res.json({ id: user.id, email: user.email });
+  }
 };
 
 export default UsersController;
