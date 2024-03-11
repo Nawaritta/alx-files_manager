aed002f94bf1f6b0aeb66ec3d703f5069efd6649
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-11 09:42:25 +0000

    Cleaning the code

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -14,31 +14,31 @@ class FilesController {
     if (!userId) return res.status(401).json({ error: 'Unauthorized' });
 
     const {
-      name, type, isPublic, data, parentId,
+      name, type, isPublic, data,
     } = req.body;
 
     if (!name) return res.status(400).json({ error: 'Missing name' });
     if (!type || !['folder', 'file', 'image'].includes(type)) return res.status(400).json({ error: 'Missing type' });
     if (!data && type !== 'folder') return res.status(400).json({ error: 'Missing data' });
 
-    let parentFile;
+    let parentId = req.body.parentId || 0;
     if (parentId) {
-      parentFile = await dbClient.dbClient.collection('files').findOne({ _id: ObjectId(parentId) });
+      const parentFile = await dbClient.dbClient.collection('files').findOne({ _id: ObjectId(parentId) });
       if (!parentFile) return res.status(400).json({ error: 'Parent not found' });
       if (parentFile.type !== 'folder') return res.status(400).json({ error: 'Parent is not a folder' });
     }
+    parentId = parentId !== 0 ? ObjectId(parentId) : 0;
 
     const folderData = {
       userId,
       name,
       type,
       isPublic: isPublic || false,
-      parentId: parentId || 0,
+      parentId,
     };
-
     if (type === 'folder') {
       const newFolder = await dbClient.dbClient.collection('files').insertOne({
-        userId, name, type, isPublic: isPublic || false, parentId: parentId || '0',
+        userId, name, type, isPublic: isPublic || false, parentId,
       });
       return res.status(201).json({ id: newFolder.insertedId, ...folderData });
     }
@@ -60,12 +60,12 @@ class FilesController {
 
     const userId = await redisClient.get(`auth_${token}`);
     if (!userId) return res.status(401).json({ error: 'Unauthorized' });
-    // catch fileId validation
+
     const fileId = req.params.id;
     const file = await dbClient.dbClient.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
 
     if (!file) return res.status(404).json({ error: 'Not found' });
-    // delete file.localPath;
+
     return res.json(file);
   }
 
@@ -77,40 +77,27 @@ class FilesController {
 
     if (!userId) return res.status(401).json({ error: 'Unauthorized' });
 
-    let parentId = req.query.parentId || '0';
+    const parentId = req.query.parentId ? ObjectId(req.query.parentId) : '0';
 
-    // const query = { userId };
-    if (parentId !== '0') {
-      parentId = ObjectId(parentId);
-    }
-
-    console.log(userId, parentId);
     const filesCount = await dbClient.dbClient.collection('files')
       .countDocuments({ userId: ObjectId(userId), parentId });
-    if (filesCount === '0') {
-      console.log('return empty list');
-      return res.json([]);
-    }
 
-    const page = parseInt(req.query.page, 10) || '0';
-    const pageSize = 20;
-    // if (page > 0) {
-    //   page -= 1; }
+    if (filesCount === '0') return res.json([]);
 
-    const skip = page * pageSize;
+    const skip = (parseInt(req.query.page, 10) || 0) * 20;
     const files = await dbClient.dbClient.collection('files')
       .aggregate([
         { $match: { userId: ObjectId(userId), parentId } },
         { $skip: skip },
-        { $limit: pageSize },
+        { $limit: 20 },
       ]).toArray();
-    // maybe a lala nora, remove localpath
-    console.log(files);
+
     const modifyResult = files.map((file) => ({
       ...file,
-      id: file._id, // rename _id to id
-      _id: undefined, // remove _id
+      id: file._id,
+      _id: undefined,
     }));
+
     return res.json(modifyResult);
   }
 }
