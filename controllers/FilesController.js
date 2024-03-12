c4394fc8ffa2a8b69432a10969e99e33afe1d5bc
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-12 03:43:14 +0000

    Fix eslint

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,9 +2,9 @@ import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import path from 'path';
 import { ObjectId } from 'mongodb';
+import mime from 'mime-types';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-import mime from 'mime-types';
 
 class FilesController {
   static async postUpload(req, res) {
